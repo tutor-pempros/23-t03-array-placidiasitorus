ba9f17d615987d0a6c1d909ef03c57a36460544a
placidiasitorus <santasitorus105@gmail.com>
2024-02-06 21:37:45 +0700

    placidia cantik

--- t03_01.c
+++ t03_01.c
@@ -5,7 +5,30 @@
 int main(int _argc, char **_argv)
 {
 
-  // your code here
+  int n,max,min,i;
+  scanf("%d", &n);
+  
+  int angka[n]
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &angka[i]);
+    if (i == 0) {
+      max = angka[i];
+      min = angka[i];
+    } else {
+      if (angka[i] > max) {
+        max = angka[i];
+      }
+      if (angka[i] < min) {
+        min = angka[i];
+      }
+    }
+ 
+  }
+  printf("%d\n" ,&min);
+  printf("%d\n" ,&max);
+
+  
+
 
   return 0;
 }
